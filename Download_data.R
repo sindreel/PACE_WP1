6c103cea85dc29a92fb2ed1b0d96d3b1118d87eb
sindreel <33902603+sindreel@users.noreply.github.com>
2022-01-31 11:04:32 +0100

    added more data

--- Download_data.R
+++ Download_data.R
@@ -27,8 +27,15 @@ URL_NALO_2021 <- "https://ntnu.box.com/shared/static/xgy66pt4eux3jolqt3avmskaby7
 download.file(url=URL_NALO_2021,destfile="./data/raw_data/NALO_2021.csv")  
 
 
-#Addirional NALO data from missing individuals from Keka 10.01.2021:
+#Additional NALO data from missing individuals from Keka 10.01.2021:
 URL_NALO_2021_keka <- "https://ntnu.box.com/shared/static/8ynblzadzj6rv23a6gfgdq13gy2ylnc6.csv"
 download.file(url=URL_NALO_2021_keka,destfile="./data/raw_data/NALO_2021_keka.csv")  
 
 
+#NALO2020-data
+URL_NALO_2020 <- "https://ntnu.box.com/shared/static/lv7vzc1823ngfagdg9gp75qbvwfw2spw.csv"
+download.file(url=URL_NALO_2020,destfile="./data/raw_data/NALO_2020.csv")  
+
+#PACE_sampling_sheets_2020
+URL_PACE_sampling_sheets_2020 <- "https://ntnu.box.com/shared/static/b7uonhrnjopblutuq6d5dewigv6un534.csv"
+download.file(url=URL_PACE_sampling_sheets_2020,destfile="./data/raw_data/PACE_WP1_Sampling_sheets_2020.csv")  
